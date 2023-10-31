92dfc9f340194bef3fb50ee10e81f936be1338d6
Samruddhi Patil <patilsamruddhi99@gmail.com>
2023-10-31 02:09:38 -0500

    Final Commit

---
+++ mysql_db.sql
@@ -0,0 +1,22 @@
+use test;
+
+create table user_data(
+    user_id int(11) primary key,
+    username varchar(20),
+    password varchar(20)
+     );
+
+create table user_saved_stock(
+    user_id int(11),
+    stocksymbol varchar(10),
+    stockprice decimal(10,2),
+    stockquantity int(5),
+    FOREIGN KEY(user_id) REFERENCES user_data(user_id),
+    FOREIGN KEY(stocksymbol) REFERENCES stock_for_cap(stocksymbol)
+    );
+    
+create table stock_for_Cap(
+    stocksymbol varchar(10) primary key,
+    marketcap varchar(20)
+    );
+          
